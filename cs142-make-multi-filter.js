a5ea7d2a1771637a038186f4a46537cd8f00a93f
uranbileg11 <60873560+uranbileg11@users.noreply.github.com>
2020-06-01 23:03:01 +0800

    Add files via upload

---
+++ cs142-make-multi-filter.js
@@ -0,0 +1,15 @@
+function cs142MakeMultiFilter(originalArray) {
+    var currentArray = originalArray;
+    function arrayFilter (filterCriteria, callback) {
+        if (typeof filterCriteria !== "function") {
+            return currentArray;
+        }        
+        
+        currentArray = currentArray.filter(filterCriteria);
+        if (typeof callback === "function") {
+            callback.call(originalArray, currentArray);
+        }
+        return arrayFilter;
+    }
+    return arrayFilter;
+}
